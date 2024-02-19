7b76c114027a77230851066ba72b7a18cdb469a6
Ilhanmuhtadin <106312966+Ilhanmuhtadin@users.noreply.github.com>
2024-02-19 22:32:00 +0700

    Update simple_streamlit_project.py

--- simple_streamlit_project.py
+++ simple_streamlit_project.py
@@ -192,11 +192,188 @@ elif contact_method == "Polynomial Regresion":
 elif contact_method == "Logistic_Regression":
     st.title('hearing test')
 
+
+    tab1, tab2, tab3,tab4,tab5 = st.tabs(["Predict Data","Simple Info Data","Distribution Data",
+                                            "Accuracy","Github"])
+    df=pd.read_csv('data/hearing_test.csv')
+    with tab1:
+        st.header("Predict Data")
+        with st.expander("Sample data"):
+            st.dataframe(df.head())
+
+        with st.form("my_form"):
+                number1 = st.number_input("age", value=None,key="1", placeholder="Type a number...")
+                number2 = st.number_input("physical_score", value=None,key="12", placeholder="Type a number...")
+
+                loaded_model = load('model/a_3_logis/C_1.599858719606058_solver_lbfgs.joblib')
+                scaler = pickle.load(open('model/a_3_logis/scaler_bin.pkl  ', 'rb'))
+            
+            # Every form must have a submit button.
+                submitted = st.form_submit_button("Submits")
+                if submitted:
+
+
+                    hasil_p=scaler.transform([[number1,number2]])
+                    hasil=loaded_model.predict(hasil_p)
+                    
+                    
+                    if hasil[0]==1:
+                        st.write(hasil[0],'your hearing is good')
+                    else:
+                        st.write(hasil[0],'your hearing is not good')
+
+
+
+    with tab2:
+        st.header("Simple Info Data")
+        
+
+        st.dataframe(df.head())
+
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
+
+
+        
+        csv = convert_df(df)
+
+
+        st.download_button(
+            label="Download full data as CSV",
+            data=csv,
+            file_name='hearing_test.csv',
+            mime='text/csv',
+        )
+
+
+
+        
+
+        st.header("describe data")
+        st.dataframe(df.describe())
+
+        st.header("info data")
+        buffer = StringIO()
+        df.info(buf=buffer)
+        s = buffer.getvalue()
+
+        st.text(s)
+
+
+
+    with tab3:
+        st.header("Distribution Data")
+        
+        
+
+
+    with tab4:
+        st.header("Accuracy")
+        st.header("MAE : 0.3926093765986013")
+        st.header("MSE : 0.2578347048485534")
+        st.header("RMSE : 0.5077742656422768")
+        
+
+
+      
+    with tab5:
+        st.header("Github")
   
 
 
 elif contact_method == "Logistic_Regression_multy_1":
     st.title('iris')
+
+
+    tab1, tab2, tab3,tab4,tab5 = st.tabs(["Predict Data","Simple Info Data","Distribution Data",
+                                            "Accuracy","Github"])
+    df=pd.read_csv('data/iris.csv')
+    with tab1:
+        st.header("Predict Data")
+        with st.expander("Sample data"):
+            st.dataframe(df.sample(5,random_state=1))
+
+        with st.form("my_form"):
+                number1 = st.number_input("sepal_length", value=None,key="1", placeholder="Type a number...")
+                number2 = st.number_input("sepal_width", value=None,key="12", placeholder="Type a number...")
+                number3 = st.number_input("petal_length", value=None,key="123", placeholder="Type a number...")
+                number4 = st.number_input("petal_width", value=None,key="1234", placeholder="Type a number...")
+                loaded_model = load('model/a_3_logis/C_316.22776601683796_solver_saga_l1_ovr.joblib')
+                scaler = pickle.load(open('model/a_3_logis/scaler_multy.pkl  ', 'rb'))
+            # Every form must have a submit button.
+                submitted = st.form_submit_button("Submits")
+                if submitted:
+                    
+
+                    hasil_p=scaler.transform([[number1,number2,number3,number4]])
+                    hasil=loaded_model.predict(hasil_p)
+              
+                    
+                   
+
+                    st.write(hasil[0])
+
+
+
+    with tab2:
+        st.header("Simple Info Data")
+        
+
+        st.dataframe(df.head())
+
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
+
+
+        
+        csv = convert_df(df)
+
+
+        st.download_button(
+            label="Download full data as CSV",
+            data=csv,
+            file_name='iris.csv',
+            mime='text/csv',
+        )
+
+
+
+        
+
+        st.header("describe data")
+        st.dataframe(df.describe())
+
+        st.header("info data")
+        buffer = StringIO()
+        df.info(buf=buffer)
+        s = buffer.getvalue()
+
+        st.text(s)
+
+
+
+    with tab3:
+        st.header("Distribution Data")
+        
+        
+
+
+
+
+    with tab4:
+        st.header("Accuracy")
+        st.header("MAE : 0.3926093765986013")
+        st.header("MSE : 0.2578347048485534")
+        st.header("RMSE : 0.5077742656422768")
+        
+
+   
+
+    with tab5:
+        st.header("Github")
+
 
 
 
