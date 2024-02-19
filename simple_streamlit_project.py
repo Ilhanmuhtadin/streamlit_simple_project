6a22361f2c168377ff4ef2a4a686939088f99b7c
Ilhanmuhtadin <106312966+Ilhanmuhtadin@users.noreply.github.com>
2024-02-19 23:03:15 +0700

    Update simple_streamlit_project.py

--- simple_streamlit_project.py
+++ simple_streamlit_project.py
@@ -16,7 +16,7 @@ import pickle
 contact_method = st.sidebar.selectbox(
     "simple Machine Learning",
     ("linear regresion","Polynomial Regresion",'Logistic_Regression','Logistic_Regression_multy_1',"KNN Classifier","SVM Classifier",
-     "SVM Regression","random forest classifier")
+     "SVM Regression","decision tree classifier","random forest classifier")
 )
 
 
@@ -779,3 +779,134 @@ elif contact_method == "random forest classifier":
 
     with tab5:
         st.header("Github")
+
+
+elif contact_method == "decision tree classifier":
+    st.title('pinguin_species')
+    
+    tab1, tab2, tab3,tab4,tab5 = st.tabs(["Predict Data","Simple Info Data","Distribution Data",
+                                            "Accuracy","Github"])
+    df=pd.read_csv('data/a_7_dtc\pinguin_ukuran.csv')
+    df_kolums=pd.read_csv('data/a_7_dtc\kolums.csv')
+    
+    
+    with tab1:
+        st.header("Predict Data")
+        with st.expander("Sample data"):
+            st.dataframe(df.sample(5,random_state=42))
+    
+        with st.form("my_form"):
+                option1 = st.selectbox(
+                'sex',
+                ('MALE', 'FEMALE'))
+    
+                option2 = st.selectbox(
+                'from island',
+                ('Torgersen', 'Biscoe', 'Dream'))
+                number1 = st.number_input(df.columns[2], value=None,key="1", placeholder="Type a number...")
+                number2 = st.number_input(df.columns[3], value=None,key="2", placeholder="Type a number...")
+                number3 = st.number_input(df.columns[4], value=None,key="3", placeholder="Type a number...")
+                number4 = st.number_input(df.columns[5], value=None,key="4", placeholder="Type a number...")
+    
+    
+                loaded_model = load('model/a_7_dtc/dtc_sc_com.joblib')
+    
+                lop=df.drop(columns='species')
+                
+                lop1=pd.DataFrame([[option2,number1,number2,number3,number4,option1]])
+                lop1.columns=lop.columns
+                df_pred=pd.concat([lop1,lop])
+                df_pred_d=pd.get_dummies(df_pred)
+                
+    
+                
+                #st.dataframe(df_pred_d)
+                kolom=df_kolums.columns
+                #st.dataframe(kolom[1:])
+                df_final=df_pred_d[kolom[1:]]
+                ff=df_final.iloc[[0]]
+    
+    
+    
+    
+                
+            # Every form must have a submit button.
+                submitted = st.form_submit_button("Submits")
+                if submitted:
+    
+    
+                    
+                    hasil=loaded_model.predict(ff)
+                    #st.dataframe(ff)
+                   
+                    
+                    if hasil[0]==1:
+                        st.write(hasil[0])
+                        #st.write(hasil[0],'your hearing is good0',kemungkinan[0][0],kemungkinan[0][1])
+                    else:
+                        #st.write(hasil[0],'your hearing is not good',kemungkinan[0][0],kemungkinan[0][1])
+                        st.write(hasil[0])
+    
+    
+    
+    with tab2:
+        st.header("Simple Info Data")
+        
+    
+        st.dataframe(df.sample(5,random_state=42))
+    
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
+    
+    
+        
+        csv = convert_df(df)
+    
+    
+        st.download_button(
+            label="Download full data as CSV",
+            data=csv,
+            file_name='pinguin_species.csv',
+            mime='text/csv',
+        )
+    
+    
+    
+        
+    
+        st.header("describe data")
+        st.dataframe(df.describe())
+        st.dataframe(df.describe(include="O"))
+        
+    
+        st.header("info data")
+        buffer = StringIO()
+        df.info(buf=buffer)
+        s = buffer.getvalue()
+    
+        st.text(s)
+    
+    
+    
+    with tab3:
+        st.header("Distribution Data")
+        
+        
+    
+    
+    
+    
+    with tab4:
+        st.header("Accuracy")
+        st.header("MAE : 0.3926093765986013")
+        st.header("MSE : 0.2578347048485534")
+        st.header("RMSE : 0.5077742656422768")
+        
+    
+       
+    with tab5:
+        st.header("Github")
+      
+    
+
